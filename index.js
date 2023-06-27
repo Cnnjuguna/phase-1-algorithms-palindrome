f7523fc723132deeae85b17e8d49d8546c39f1e4
njugudev <njugudev@pop-os.localdomain>
2023-06-27 08:38:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const revWord = word.toLowerCase().split('').reverse().join('');
+  return word === revWord;
 }
 
 /* 
   Add your pseudocode here
+  // Create a new word reversing variable (revWord)
+  // Take the word and split it into its individual characters.
+  // Use .split as this assigns each character a value.
+  // use .reverse to reverse the order of the characters.
+  // use.join to join the characters back together.
+  // check whether the reversed word is the same as the original
 */
 
 /*
